63b318edcde8b3c9b6982ab9859781ec9f6ab22f
Mich_H <hruzko@gmail.com>
2023-11-28 16:16:50 +0100

    Used tiles implementation
    Methods and use

---
+++ azul/usedTiles.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+from typing import List
+from simple_types import Tile, compress_tile_list, Points, RED, BLUE, YELLOW, GREEN, BLACK
+
+class usedTiles():
+    def __init__(self) -> None:
+        self.tiles: list[Tile] = []
+    
+    def state(self) -> str:
+        return "".join([str(x) for x in self.tiles])
+    
+    def give(self, tiles: list[Tile]) -> None:
+        for tile in tiles: 
+            if tile.__str__() != "S":
+                self.tiles.append(tile)
+        
+    def takeAll(self) -> list[Tile]:
+        result: list[Tile] = self.tiles
+        self.tiles = []
+        return result
