d2428e5dcd8e6f1873f82afad44e10b1c31a53ae
Mich_H <60292048+Samuel6789@users.noreply.github.com>
2023-11-29 10:35:51 +0100

    Delete azul/game_observer.py
    pushed on wrong branch

--- azul/game_observer.py
+++
@@ -1,8 +0,0 @@
-from interfaces import ObserverInterface
-
-class GameObserver:
-    def __init__(self) -> None:
-        pass
-
-    def notifyEverybody(state:str) -> None:
-        ObserverInterface.notify(state)
