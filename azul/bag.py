0e8b6680b3ec076cba7ad6c1d58875ef1ae8f490
Mich_H <hruzko@gmail.com>
2023-11-29 14:15:03 +0100

    Added bag and factory form local
    Tests will be added in next commit. Changed from using interfaces to implemented classes.

---
+++ azul/bag.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+from typing import List
+from simple_types import Tile, compress_tile_list, Points, RED, BLUE, YELLOW, GREEN, BLACK
+import random
+
+class Bag:
+    def __init__(self) -> None:
+        self.tileCount: int = 100
+        self.tiles: list[Tile] = []
+        for i in range(20):
+            self.tiles.append(RED)
+            self.tiles.append(BLUE)
+            self.tiles.append(YELLOW)
+            self.tiles.append(GREEN)
+            self.tiles.append(BLACK)
+        self.shuffle()
+    
+    def state(self) -> str:
+        return "".join([str(x) for x in self.tiles])
+    
+    def take(self, count: int) -> list[Tile]:
+        if count < 0 or count > self.tileCount:
+            return []
+        result: list[Tile] = []
+        for i in range(count):
+            self.tileCount -= 1
+            result.append(self.tiles.pop(0))
+        return result
+    
+    def shuffle(self) -> None:
+        for i in range(2*len(self.tiles)):
+            randomx: int = random.randint(0, len(self.tiles) - 1)
+            randomy: int = random.randint(0, len(self.tiles) - 1)
+            temp: Tile = self.tiles[randomx]
+            self.tiles[randomx] = self.tiles[randomy]
+            self.tiles[randomy] = temp
+        
+
+
+
---
+++ azul/factory.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+from typing import List
+from simple_types import Tile, compress_tile_list, Points, RED, BLUE, YELLOW, GREEN, BLACK
+from bag import Bag
+from tileSources import TileSource
+from tileSources import tableCenter
+
+
+class Factory(TileSource):
+    def __init__(self, bag: Bag, center: tableCenter) -> None:
+        super().__init__()
+        self.bag: Bag = bag 
+        self.center: tableCenter = center
+    
+    def startNewRound(self) -> None:
+        self.tiles += self.bag.take(4)
+    
+    def take(self, colour: int) -> list[Tile]:
+        result: list[Tile] = super().take(colour)
+        if result == []:
+            return result
+        self.center.add(self.tiles)
+        self.tiles.clear()
+        return result
