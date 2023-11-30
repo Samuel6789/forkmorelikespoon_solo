4c42e0e0ab7398216cf0ac8d8f0fd4998948728b
Mich_H <60292048+Samuel6789@users.noreply.github.com>
2023-11-30 14:49:16 +0100

    Update finalPointsCalculation.py
    According to group repo

--- azul/finalPointsCalculation.py
+++ azul/finalPointsCalculation.py
@@ -1,22 +1,19 @@
 from collections import Counter
-from simple_types import Points
+from azul.simple_types import Points, Tile
 from typing import List, Any
 
 
 class FinalPointsCalculation:
     @staticmethod
-    def calculate_points(wall: List[List[Any]]):
+    def calculate_points(wall: List[List[Any]]) -> Points:
         points: int = 0
-        colours_counter: Counter[str] = Counter()
+        colours_counter: Counter[Any] = Counter()
         for row in wall:
-            row: List[str] = list(filter(lambda x: x is not None, row))
-            row: List[str] = list(map(lambda x: str(x), row))
             colours_counter.update(row)
-            if len(row) == 5:
+            if len(list(map(lambda x: str(x), list(filter(lambda x: x is not None, row))))) == 5:
                 points += 2
         for column in zip(*wall):
-            column: List[str] = list(filter(lambda x: x is not None, column))
-            if len(column) == 5:
+            if len(list(filter(lambda x: x is not None, column))) == 5:
                 points += 7
         del colours_counter[None]
         for count in colours_counter.values():
